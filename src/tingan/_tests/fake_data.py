0c82fb6fd5c99d283acc6b06b17a4e4290291d5f
Stephen Thompson <s.thompson@ucl.ac.uk>
2026-08-20 10:19:01 +0100

    messing with pathlib

--- src/tingan/_tests/fake_data.py
+++ src/tingan/_tests/fake_data.py
@@ -94,7 +94,7 @@ def fake_concatenated_file(n_files: int, string: str = "PSR") -> Path:
     temp_path = os.environ.get("RUNNER_TEMP")
     path = Path(f"{temp_path}/data/fake/")
     path.mkdir(parents=True, exist_ok=True)
-    path = Path(path + f"concat_{string}.txt")
+    path = path / f"concat_{string}.txt"
     with path.open("w") as f:
         for i in range(n_files):
             f.write(string + "\n")
